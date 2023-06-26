12d6020dee2a8fe92feef74929a14ba098d7b7be
Mehmet Nuri <64698532+mmetnuri@users.noreply.github.com>
2023-06-26 15:33:39 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./app.css";
+import Table from "./Table";
+import { Users } from "./users";
+
+function App() {
+  
+  const [query, setQuery] = useState("");
+
+  const keys = ["first_name", "last_name", "email"];
+
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search.."
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <Table data={search(Users)} />
+    </div>
+  );
+}
+
+export default App;
